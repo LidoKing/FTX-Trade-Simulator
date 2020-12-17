32b0ab9e48e41347b4fd36f22608e713646273f0
LidoKing <kinglido203@gmail.com>
2020-12-18 00:23:59 +0800

    Parsed JSON to JS object to get price of LINKBULL

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // Create a request variable and assign a new XMLHttpRequest object to it
-var request = new XMLHttpRequest()
+let request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ftx.com/api/lt/LINKBULL', true)
+request.open('GET', 'https://ftx.com/api/lt/LINKBULL', true);
 
-request.onload = function () {
+request.onload = () => {
   // Begin accessing JSON data here
 }
 
 // Send request
 request.send()
+
+
+// Begin accessing JSON data here
+// Convert JSON to JavaScript objects
+let data = JSON.parse(this.response);
+
+console.log(data.pricePerShare)
